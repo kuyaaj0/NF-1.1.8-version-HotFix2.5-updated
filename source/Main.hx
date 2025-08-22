d59669e4af79847f6cc686b947e2c4c0e7fca12e
beihu235 <2120024137@qq.com>
2025-08-22 17:26:00 +0800

    尝试安卓使用haxeui

--- source/Main.hx
+++ source/Main.hx
@@ -134,9 +134,7 @@ class Main extends Sprite
 			game.height = Math.ceil(stageHeight / game.zoom);
 		}
 
-		#if desktop
 		Toolkit.init();
-		#end
 
 		#if LUA_ALLOWED llua.Lua.set_callbacks_function(cpp.Callable.fromStaticFunction(psychlua.CallbackHandler.call)); #end
 		Controls.instance = new Controls();
