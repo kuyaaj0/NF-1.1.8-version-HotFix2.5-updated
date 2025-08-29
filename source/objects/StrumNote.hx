558d44522ac79255dd1f2af66182617e16c3c6ad
kangel <132186966+kangeluwu@users.noreply.github.com>
2025-08-29 19:38:40 +0800

    Note.init你赢了

--- source/objects/StrumNote.hx
+++ source/objects/StrumNote.hx
@@ -259,6 +259,7 @@ class StrumNote extends FlxSprite
 			}
 		}
 	}
+	
 	public function getIndex(mania:Int, note:Int) {
 		return ExtraKeysHandler.instance.data.keys[mania].notes[note];
 	}
--- source/options/base/NotesSubStateLegacy.hx
+++ source/options/base/NotesSubStateLegacy.hx
@@ -47,7 +47,8 @@ class NotesSubStateLegacy extends MusicBeatSubstate
         #if DISCORD_ALLOWED
 		DiscordClient.changePresence("Note Colors Menu", null);
 		#end
-		
+		Note.init();
+
 		var bg:FlxSprite = new FlxSprite(0, 0).makeGraphic(FlxG.width, FlxG.height, FlxColor.WHITE);
 		bg.scrollFactor.set();
 		bg.alpha = 0.5;
